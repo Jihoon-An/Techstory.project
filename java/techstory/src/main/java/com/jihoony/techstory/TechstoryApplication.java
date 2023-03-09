8db84a2ff4348908c3a69c797d9a0c13f500104a
dOcOb <abg1418@gmail.com>
2023-03-09 23:52:52 +0900

    [Feat] Create java project

---
+++ java/techstory/src/main/java/com/jihoony/techstory/TechstoryApplication.java
@@ -0,0 +1,13 @@
+package com.jihoony.techstory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechstoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechstoryApplication.class, args);
+	}
+
+}
---
+++ java/techstory/src/test/java/com/jihoony/techstory/TechstoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jihoony.techstory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechstoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
